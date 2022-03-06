21a417e50a2c4a47bc6828bd7dbccfcc1bfe15ec
qkrwjdan <madogisa12@naver.com>
2022-03-06 21:16:21 +0900

    new commit

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.get("/", function(req, res){
     res.send("Hello World!.");
 })
 
+app.get("/new", function(req, res){
+    res.send("새로 배포된 페이지입니다.");
+})
+
 app.listen(process.env.PORT || 5000);
 
 module.exports = app;
